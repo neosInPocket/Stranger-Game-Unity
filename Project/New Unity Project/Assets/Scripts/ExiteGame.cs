d86cd7f1c8c217dc4bbbc4f40bd365167cfed9b3
AliaksandrParetski <aliaksandrparetski@gmail.com>
2022-09-06 23:30:21 +0300

    добавил нпс и анимацию к нему

---
+++ Project/New Unity Project/Assets/Scripts/ExiteGame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExiteGame : MonoBehaviour
+{
+    public void Exite_Game()
+    {
+        Application.Quit();
+    }
+}
