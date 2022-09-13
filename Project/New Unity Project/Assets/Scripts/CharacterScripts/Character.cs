e4eaab5be8b8dbd5730370a4650cbf3c9ed68744
AliaksandrParetski <aliaksandrparetski@gmail.com>
2022-09-14 00:05:35 +0300

    Изменение
    Добавил гланого персонажа анимировал его

---
+++ Project/New Unity Project/Assets/Scripts/CharacterScripts/Character.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Animator animator;
+
+    protected Vector2 direction;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    protected virtual void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (direction.x != 0 || direction.y != 0)
+        {
+            AnimateMovement(direction);
+        }
+    }
+
+    public void AnimateMovement(Vector2 direction)
+    {
+        animator.SetFloat("x", direction.x);
+        animator.SetFloat("y", direction.y);
+
+    }
+}
