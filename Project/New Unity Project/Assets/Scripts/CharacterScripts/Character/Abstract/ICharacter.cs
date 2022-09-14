2eea7828ef7caab392a8f17982cb45d773fe7321
neosInPocket <fortpost197080@gmail.com>
2022-09-14 21:44:46 +0300

    Revert "Character changes"
    This reverts commit 78602c467250d4d772c4cdaf2e8c557dbbd6f541.

--- Project/New Unity Project/Assets/Scripts/CharacterScripts/Character/Abstract/ICharacter.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ICharacter
-{
-    public float Health { get; set; }
-    public float Weaopn { get; set; }
-}
