c9736e66dcb955e040f733ea8bb33918674e32c2
AliaksandrParetski <aliaksandrparetski@gmail.com>
2022-09-26 23:04:32 +0300

    ысыв
    ывсмывмы

--- Project/New Unity Project/Assets/Scripts/Quest/Quest.cs
+++ Project/New Unity Project/Assets/Scripts/Quest/Quest.cs
@@ -1,4 +1,5 @@
 using Assets.Scripts.Inventory;
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -106,6 +107,7 @@ public abstract class Objective
 [Serializable]
 public class CollectObjective : Objective
 {
+<<<<<<< Updated upstream
     [SerializeField] private GameObject go;
     private UIInventory inv;
     private int objectiveAmount;
@@ -134,6 +136,11 @@ public class CollectObjective : Objective
     void QuestEnd()
     {
         var slots = inv.inventory.GetAllSlots();
+=======
+    public void UpdateItemCount(IInventoryItem item)
+    {
+        item.info.
+>>>>>>> Stashed changes
     }
 }
 
